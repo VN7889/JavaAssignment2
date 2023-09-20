9a834084918643c78475b9fc4b7b905b2448009d
Varun N <103560317+VN7889@users.noreply.github.com>
2023-09-19 23:19:55 -0400

    Delete HelloWorld10Times.java

--- HelloWorld10Times.java
+++
@@ -1,9 +0,0 @@
-package com.basics;
-
-public class HelloWorld10Times {
-    public static void main(String args[]) {
-        for(int i=0;i<10;i++) {
-            System.out.println("Hello World");
-        }
-    }
-}
